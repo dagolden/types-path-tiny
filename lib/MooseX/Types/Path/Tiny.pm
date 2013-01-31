16376520d2eb25cf35addc8d89766cfe944e53ff
David Golden <dagolden@cpan.org>
2013-01-31 06:05:41 -0500

    import

---
+++ lib/MooseX/Types/Path/Tiny.pm
@@ -0,0 +1,35 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package MooseX::Types::Path::Tiny;
+# ABSTRACT: No abstract given for MooseX::Types::Path::Tiny
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+=for Pod::Coverage method_names_here
+
+=head1 SYNOPSIS
+
+  use MooseX::Types::Path::Tiny;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
